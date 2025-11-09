6a565bff26ee6b1ed135d494014d604d5f476fc0
Aashika <khatiwadaaashika12@gmail.com>
2025-11-09 20:23:56 +0545

    sql

--- createinsert_aashika_01/src/App.java
+++ createinsert_aashika_01/src/App.java
@@ -7,12 +7,14 @@ public class App {
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/swastikc?useSSL=false","root","");
-            String sql="create table tblStudent(
-            id int primary key identity,
-            name varchar 50,
-            email varchar 50);";
-            PreparedStatement ps = con.prepareStatement(updatesql);
+            String sql="create table tblStudent(id INT PRIMARY KEY AUTO_INCREMENT,name VARCHAR(50),email VARCHAR(50));";
+            PreparedStatement ps = con.prepareStatement(sql);
             ps.executeUpdate();
+            String insertSql="insert into tblStudent(name,email) values(?,?)";
+            PreparedStatement ips= con.prepareStatement(insertSql);
+            ips.setString(1, "Anna Thapa");
+            ips.setString(2, "annat2@gmail.com");
+            ips.executeUpdate();
             con.close();
             System.out.println("Salary updated successfully");
         }
