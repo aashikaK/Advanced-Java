677861c0804ee7f0d2301c94da1d073b5fa1f3c4
Aashika <khatiwadaaashika12@gmail.com>
2025-11-09 19:26:55 +0545

    insert

--- insertjdbc_aashika_01/src/App.java
+++ insertjdbc_aashika_01/src/App.java
@@ -7,18 +7,28 @@ public class App {
     public static void main(String[] args) throws Exception {
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/gym?useSSL=false","root","");
-            
-       String insertsql="insert into users_login(username,password,email) values(?,?,?)";
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/swastikc?useSSL=false","root","");
+            String insertsql="insert into student(name,gender,address) values(?,?,?)";
             PreparedStatement ps = con.prepareStatement(insertsql);
-            for(int i=0;i<5;i++){
+            Scanner sc= new Scanner(System.in);
+            for (int i = 0; i < 5; i++) {
+                System.out.println("\nEnter details of student " + (i + 1));
+                System.out.print("Name: ");
+                String name = sc.nextLine();
 
+                System.out.print("Gender: ");
+                String gender = sc.nextLine();
+
+                System.out.print("Address: ");
+                String address = sc.nextLine();
+
+                ps.setString(1, name);
+                ps.setString(2, gender);
+                ps.setString(3, address);
+                ps.executeUpdate();
             }
-            ps.setString(1, "Bimala");
-            ps.setString(2, "bimala12");
-            ps.setString(3, "bimala2@gmail.com");
-            ps.executeUpdate();
             con.close();
+            sc.close();
             System.out.println("Data Inserted");
         }
         catch(Exception e){
